6a6338349247be6980e0b07ca9ff0b9bec9fa1c2
Helmet Coding <126153404+oscarhelmet@users.noreply.github.com>
2023-08-02 11:41:35 +0800

    NotUsefulTool.py

---
+++ NotUsefulTool.py
@@ -0,0 +1,24 @@
+import time 
+import random
+
+
+print("Starting training model 'DBNet_Text_Rec_Big5', using GPU #0 - NVIDA RTX 3090 TI, VRAM Alloc: 16Gib")
+
+# Try to print a fake training epoch 
+def train():
+    n = 1
+    while n < 200:
+        for i in range(1339):
+            #print(f"Epoch {n}:" +'{0: <20}'.format("="* int(i/50), end=">")    + f"|  {i}/1000" + f" losses:{random.uniform(0.09,0.08)}, ", end='\r', flush=True)
+            print(f"Epoch {n}:[" + '=' * int(i / 33) + '>' + ' ' * (40 - int(i / 33)) + f"]  {i}/1338" + f" losses:{(1.55/n)*random.uniform(0.88,1.08):.4f} - binary classification val-accracy:{100-(1.55/n)*random.uniform(0.88,1.08):.4f}", end='\r',flush=True)
+            time.sleep(0.02)
+        print(f"\r\nFinished {n} Epoch, the val-accuracy after this epoch: {random.uniform(98.92, 99.99):.4f}")
+        n+=1
+
+while True:
+    train()
+    print("Finish Training. Saving the model....")
+
+print("I have no idea why this code is written")
+# probably I wanna sleep in the office and act like the model is training.
+# I am clever, haha
